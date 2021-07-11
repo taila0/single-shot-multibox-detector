33b844c214ae0637b9d7b70141e89cdff9e0dcd1
pai-plznw4me <plznw4me@publicai.co.kr>
2021-07-11 10:59:50 +0900

    main 함수를 generate_tiling_default_boxes 을 사용하도록 변경

--- src/default_boxes.py
+++ src/default_boxes.py
@@ -138,78 +138,73 @@ def original_rectangle_coords(fmap_size, kernel_sizes, strides, paddings):
     return coords
 
 
-def generate_tiling_default_boxes(*args, **kwargs):
+def generate_tiling_default_boxes(**kwargs):
     """
     Description:
         위 함수는 아래 순서로 작동 합니다.
-        1. 모든 feature map cell 을 original image 의 원 좌표(cx, cy, w, h)로 변환
-        2. default boxes 생성
-        3. default boxes 을 1번에서 변환된 좌표에 적용
-
-        각 파트의 자세한 설명은 아래 주석에 나와 있습니다.
-        1.
-        주어진 Feature map의 center x, center y 좌표를 Original Image center x, center y에 맵핑합니다.
-        (아래 코드에서 사용된 공식은 "A guide to convolution arithmetic for deep learning" 에서 참조함)
-        2.
-        지정된 크기(scales)와 비율(ratio)에 대한 복수개의 bounding box 을 생성합니다.
-        3.
-        original image 와 좌표 위치가 매칭된 feature map의 모든 cell에 적용된 default boxes의 좌표값을 반환합니다.
+        1.  모든 feature map cell 을 original image 의 원 좌표(cx, cy, w, h)로 변환
+            주어진 Feature map의 center x, center y 좌표를 Original Image center x, center y에 맵핑합니다.
+            (아래 코드에서 사용된 공식은 "A guide to convolution arithmetic for deep learning" 에서 참조함)
+        2.  default boxes 생성
+            지정된 크기(scales)와 비율(ratio)에 대한 복수개의 bounding box 을 생성합니다.
+        3.  default boxes 을 1번에서 변환된 좌표에 적용
+            각 파트의 자세한 설명은 아래 주석에 나와 있습니다.
 
     Args:
         :param fmap_size: tuple or list or ndarray, shape (2=(height, width)), 최종 출력된 feature map 의 크기
             example) (4, 4)
-
         :param paddings: tuple or list, [str, str, ..., str],
             feature map을 생성하기 까지의 적용된 padding,
             paddings 의 element 개 수는 layer 의 깊이와 같아야 합니다.(pooling layer 포함)
             List 의 Element 가 반드시 'SAME' 또는 'VALID' 로 구성되어 있어야 함
             example) ['SAME', 'SAME', ... 'VALID']
-
         :param strides: tuple or list or ndarray, shape (N_layer), [int, int, ... int]
             tuple or list, 각 Layer 에 적용된 stride 크기(pooling layer 포함)
             example) [2, 4, ... 3 ]
-
         :param kernel_sizes: tuple or list or ndarray, shape (N_layer), [int, int, ... int]
             feature map을 생성하기 까지의 적용된 kernel_size(pooling layer 포함)
             example) [3, 3, ... 3 ]
-
         :param scales: tuple or list, shape=(n_shape, ), (int, int, ... int )
             example) (3, 6, 9)
             ratio 가 1 일때 default 박스의 size 크기
         :param ratios: tuple or list, ((H_ratio, W_ratio), (H_ratio, W_ratio) ... (H_ratio, W_ratio)) , shape=(n_ratio, 2)
             default boxes의 h, w 정보가 순차적으로 들어있는 자료구조.
             example) ((1, 0.5), (1, 1), ... (0.5, 1))
-        :param center_xy: shape=(N, 2), feature map의 각 cell과 original image에 매칭되는 좌표
-        :param sizes: tuple, shape=(n_scales, n_ratios, 2), ratio에 scale 이 곱해진 결과값을 반환
-
     Returns:
+        :return: ndarray, (N_center, N_size(=n_scales * n_ratios), 4=(cx cy w h))
 
     """
-    kernel_sizes, strides, paddings = kwargs['kernel_sizes'], kwargs['kernel_sizes'], kwargs['kernel_sizes'],
-    center_xy = original_rectangle_coords((32, 32), kernel_sizes, strides, paddings)[:, :2]
+
+    # 주어진 Feature map의 center x, center y 좌표를 Original Image center x, center y에 맵핑합니다.
+    fmap_size = kwargs['fmap_size']
+    kernel_sizes = kwargs['kernel_sizes']
+    strides = kwargs['strides']
+    paddings = kwargs['paddings']
+    center_xy = original_rectangle_coords(fmap_size, kernel_sizes, strides, paddings)[:, :2]
+
+    # 지정된 크기(scales)와 비율(ratio)에 대한 복수개의 bounding box 을 생성합니다.
+    scales = kwargs['scales']
+    ratios = kwargs['ratios']
+    default_boxes_sizes = generate_default_boxes(scales, ratios)
+
+    # original image 와 좌표 위치가 매칭된 feature map의 모든 cell에 적용된 default boxes의 좌표값을 반환합니다.
+    default_boxes = tiling_default_boxes(center_xy, default_boxes_sizes)
+    return default_boxes
 
 
 if __name__ == '__main__':
-    """
-    feature map 의 각 cell의 center을 original image center에 맵핑함.
-        example) [[cx cy  w  h], [[cx cy  w  h]] -> [[cx cy], [cx, cy]], shape (height * width, 2)
-    get each cell center x, center y coordinates for original image
-    """
-    fmap = tf.constant(shape=(2, 32, 32, 2), value=1)
+    fmap_size = (32, 32)
     n_layer = 7
     paddings = ['SAME'] * n_layer
-    strides = [1, 1, 1, 1, 2, 1, 2]
     kernel_sizes = [3] * n_layer
-    center_xy = original_rectangle_coords((32, 32), kernel_sizes, strides, paddings)[:, :2]
-
-    # get w, h
+    strides = [1, 1, 1, 1, 2, 1, 2]
     scales = [10, 25, 40]
-    ratios = [(1, 1),
-              (1.5, 0.5),
-              (1.2, 0.8),
-              (0.8, 1.2),
-              (1.4, 1.4)]
-    sizes = generate_default_boxes(scales, ratios)
+    ratios = [(1, 1), (1.5, 0.5), (1.2, 0.8), (0.8, 1.2), (1.4, 1.4)]
 
     # Get default boxes over feature map
-    default_boxes = tiling_default_boxes(center_xy, sizes)
+    default_boxes_ = generate_tiling_default_boxes(fmap_size=(32, 32),
+                                                   paddings=paddings,
+                                                   strides=strides,
+                                                   kernel_sizes=kernel_sizes,
+                                                   scales=scales,
+                                                   ratios=ratios)
