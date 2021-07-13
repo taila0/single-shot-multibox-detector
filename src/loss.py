12386150cf3967140665bce90e69f42fa6ff1ded
seongjungkim <plznw4me@naver.com>
2021-07-13 12:44:56 +0900

    add loss 추가

--- src/loss.py
+++ src/loss.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.losses import MSE, CategoricalCrossentropy
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
 
 def detection_loss(y_true, y_pred):
@@ -68,3 +69,50 @@ def detection_loss(y_true, y_pred):
 
     loss = cls_loss + reg_loss
     return loss
+
+
+def ssd_loss(y_true, y_pred):
+    pos_neg_ratio = 3.
+    num_classes = tf.shape(y_true)[2] - 4
+    y_true = tf.reshape(y_true, [-1, num_classes + 4])
+    y_pred = tf.reshape(y_pred, [-1, num_classes + 4])
+    eps = K.epsilon()
+
+    # Split Classification and Localization output
+    y_true_clf, y_true_loc = tf.split(y_true,
+                                      [num_classes, 4],
+                                      axis=-1)
+    y_pred_clf, y_pred_loc = tf.split(y_pred,
+                                      [num_classes, 4],
+                                      axis=-1)
+
+    # split foreground & background
+    neg_mask = y_true_clf[:, -1]
+    pos_mask = 1 - neg_mask
+    num_pos = tf.reduce_sum(pos_mask)
+    num_neg = tf.reduce_sum(neg_mask)
+    num_neg = tf.minimum(pos_neg_ratio * num_pos, num_neg)
+
+    # softmax loss
+    y_pred_clf = K.clip(y_pred_clf, eps, 1. - eps)
+    clf_loss = -tf.reduce_sum(y_true_clf * tf.log(y_pred_clf),
+                              axis=-1)
+    pos_clf_loss = tf.reduce_sum(clf_loss * pos_mask) / (num_pos + eps)
+    neg_clf_loss = clf_loss * neg_mask
+    values, indices = tf.nn.top_k(neg_clf_loss,
+                                  k=tf.cast(num_neg, tf.int32))
+    neg_clf_loss = tf.reduce_sum(values) / (num_neg + eps)
+    clf_loss = pos_clf_loss + neg_clf_loss
+
+    # smooth l1 loss
+    l1_loss = tf.abs(y_true_loc - y_pred_loc)
+    l2_loss = 0.5 * (y_true_loc - y_pred_loc) ** 2
+    loc_loss = tf.where(tf.less(l1_loss, 1.0),
+                        l2_loss,
+                        l1_loss - 0.5)
+    loc_loss = tf.reduce_sum(loc_loss, axis=-1)
+    loc_loss = tf.reduce_sum(loc_loss * pos_mask) / (num_pos + eps)
+
+    # total loss
+
+    return clf_loss + loc_loss
