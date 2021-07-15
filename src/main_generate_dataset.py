19f5921236c832dfbd0f4f2d2bd2b4d70a1a757b
seongjungkim <plznw4me@naver.com>
2021-07-15 15:01:57 +0900

    fmap 크기를 자동으로 가져오는것에서 수동으로 입력하는것으로 변경

--- src/main_generate_dataset.py
+++ src/main_generate_dataset.py
@@ -6,7 +6,6 @@ from default_boxes import generate_tiling_default_boxes
 from tqdm import tqdm
 from time import time
 import numpy as np
-import tensorflow as tf
 import pickle
 
 # Load dataset
@@ -18,17 +17,14 @@ n_classes = 10 + 1
 
 # Generate detection SSD model
 n_boxes = 5
-inputs, (cls3_5, loc3_7), (cls4_5, loc4_7), (cls5_5, loc5_7) = simple_detection_netowrk(gt_img.shape,
-                                                                                        n_boxes,
-                                                                                        n_classes=11)
-multi_head_cls = [cls3_5, cls4_5, cls5_5]
-multi_head_loc = [loc3_7, loc4_7, loc5_7]
-n_head = len(multi_head_loc)
-
 # 각 header 는 하나의 scale 을 사용함, ratio 는 공유
+
+
+fmaps = [(32, 32), (16, 16), (8, 8)]
+n_head = len(fmaps)
 scales = [10, 25, 40]
 ratios = [(1, 1), (1.5, 0.5), (1.2, 0.8), (0.8, 1.2), (1.4, 1.4)]
-assert len(multi_head_cls) == len(multi_head_loc) == len(scales)
+
 
 # stem layer , block 2 마지막 layer 까지 layer argument 정의(※ 모든 multi head 는 root 을 공유함)
 stem_n_layer = 5
@@ -52,8 +48,7 @@ default_boxes_bucket = []
 # 모든 multi head 에서 default boxes 을 구합니다.
 for head_ind in tqdm(range(n_head)):
     # get feature map size
-    trgt_fmap_size = multi_head_cls[head_ind].get_shape()[1:3]
-
+    trgt_fmap_size = fmaps[head_ind]
     trgt_scale = scales[head_ind]
     trgt_paddings = stem_paddings + block_padding * (head_ind + 1)
     trgt_kernel_sizes = stem_kernel_sizes + block_kernel_sizes * (head_ind + 1)
