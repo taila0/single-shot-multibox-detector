9d871286b9ea4a724755c9cce9a16a404a6a05ca
pai-plznw4me <plznw4me@publicai.co.kr>
2021-07-25 13:59:13 +0900

    non maximun suppression 추가

--- src/nms.py
+++ src/nms.py
@@ -71,19 +71,25 @@ def non_maximum_suppression(bboxes, preds, threshold):
     :return:
         final_bboxes : list, 겹쳐진 박스가 모두 제거된 최종 박스
         final_preds : list, 겹쳐진 박스가 모두 제거된 최종 박스의 확률값
+        final_labels : list, 겹쳐진 박스가 모두 제거된 최종 박스의 class
     """
 
     # class 중 확률값이 가장 큰 class 을 선택합니다.
     # shape (N, 4) -> (N, )
+    labels = np.argmax(preds, axis=-1)
     preds = np.max(preds, axis=-1)
+
 
     # prediction 및 bounding boxes을 prediction 값이 높은 순서로 정렬(내림 차순)
     sorted_index = np.argsort(preds)[::-1]
     bboxes = bboxes[sorted_index]
     preds = preds[sorted_index]
+    labels = labels[sorted_index]
 
     final_bboxes = []
     final_preds = []
+    final_labels = []
+
     # bounding boxes에 아무것도 없을때까지 수행
     while bboxes.tolist():
         # prediction 값이 가장 높은 bounding box 후보군을 선택하고 final bboxes 집어 넣음.
@@ -91,10 +97,13 @@ def non_maximum_suppression(bboxes, preds, threshold):
         final_bboxes.append(trgt_bbox)
         trgt_pred = preds[0]
         final_preds.append(trgt_pred)
+        trgt_labels = labels[0]
+        final_labels.append(trgt_labels)
 
         # 후보 bounding box을 bboxes 에서 제거함
         bboxes = np.delete(bboxes, 0, axis=0)
         preds = np.delete(preds, 0, axis=0)
+        labels = np.delete(labels, 0, axis=0)
 
         # 후보 bbox 와 bboxes 와의 iou 을 계산함.
         ious = calculate_iou(trgt_bbox[None], bboxes)
@@ -103,5 +112,6 @@ def non_maximum_suppression(bboxes, preds, threshold):
         overlay_index = np.where(np.squeeze(ious > threshold))
         bboxes = np.delete(bboxes, overlay_index, axis=0)
         preds = np.delete(preds, overlay_index, axis=0)
+        labels = np.delete(labels, overlay_index, axis=0)
 
-    return final_bboxes, final_preds
+    return final_bboxes, final_preds, final_labels
